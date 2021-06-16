a220bf60785ba171a36452a43867bd467a346773
Comisa <com1sa@crossbow.dreamhost.com>
2021-06-15 22:03:47 -0700

    added data expiracao

--- login.php
+++ login.php
@@ -16,23 +16,29 @@ if ($system->isLogado()) {
 $cliente = new ClienteBO();
 
 if (isset($_POST)) {
-  $acao = strtoupper($_POST['acao']);
-  switch ($acao) {
+    $acao = strtoupper($_POST['acao']);
+    switch ($acao) {
     case "ACESSAR":
-      if ($cliente->LoadByLogin($_POST["login"], $_POST["senha"])) {
-        $cliente->setQtdeAcesso($cliente->getQtdeAcesso() + 1);
-        $cliente->setUltimoAcesso(date('Y-m-d H:i:s'));
-        $cliente->Save();
-        $system->setUsuarioLogado($cliente->getIdCliente(), $cliente->getNome(), $cliente->isAdmin());
-        $system->redireciona("/index.php");
-      } else {
-        $cliente->Error("Login/Senha invalidos.", null);
-      }
-      break;
+        if ($cliente->LoadByLogin($_POST["login"], $_POST["senha"])) {
+            if(date('Y-m-d') >= '2018-10-16') {
+                $cliente->Error("<strong>Plano suspenso!</strong> Entre em contato através do email gladyston.batista@stonetech.info", null);
+            }	
+            else {
+                $cliente->setQtdeAcesso($cliente->getQtdeAcesso() + 1);
+                $cliente->setUltimoAcesso(date('Y-m-d H:i:s'));
+                $cliente->Save();
+                $system->setUsuarioLogado($cliente->getIdCliente(), $cliente->getNome(), $cliente->isAdmin());
+                $system->redireciona("/index.php");
+            }
+        }
+        else {
+            $cliente->Error("Login/Senha invalidos.", null);
+        }
+        break;
 
     default:
-      break;
-  }
+        break;
+    }
 }
 $smarty = new SmartyComisa();
 $smarty->assign("objeto", $cliente);
